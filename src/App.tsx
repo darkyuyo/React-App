ec4657ddc339270c452397d1deb2434ce4953bba
darkyuyo <alfredo.llanos@sansano.usm.cl>
2024-11-21 21:20:24 -0300

    terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const list = ["Goku", "Tanjiro", "Eren"];
+  //const [isLoading, setIsLoading] = useState(false);
+  //const handleClick = () => setIsLoading(!isLoading);
+  const [list, setList] = useState(["Goku", "Tanjiro", "Eren"]);
+  const addMinion = () => setList([...list, "Minion"]);
+  const delMinion = () => setList(list.slice(0, -1));
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+  const contenido = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect} />
+  );
   return (
     <Card>
+      <Button onClick={addMinion}>Agregar</Button>
+      <Button onClick={delMinion}>Quitar</Button>
       <CardBody title="Hola mundo" text="Este es el texto" />
-      <List data={list} />
+      {contenido}
     </Card>
   );
 }
